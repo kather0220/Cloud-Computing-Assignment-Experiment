af11a0d379a73cc7696cab93cc57fe3c6f929982
kather0220 <78165538+kather0220@users.noreply.github.com>
2022-02-15 00:45:06 +0900

    Delete FindPoint directory

--- FindPoint/src/main/java/MyLog.java
+++
@@ -1,9 +0,0 @@
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-public class MyLog {
-	public Logger logger;
-	public MyLog() {
-
-		this.logger = LoggerFactory.getLogger(this.getClass());
-	}
-}
